d1520fe41db24495044cb99c434ff94b7ea86cf3
Jesse Schulman <jesse@caleos.io>
2025-01-03 12:52:09 -0800

    Adding contract addresses

--- ui/src/config.ts
+++ ui/src/config.ts
@@ -16,20 +16,20 @@ export interface AddressConfig {
 // export the list of contracts for each network
 export const contractAddressForChain: AddressConfig = {
   [mainnet.id]: {
-    oftToken: '0x111',
-    pToken: '0x111',
-    redeem: '0x111',
+    oftToken: '0x193f4A4a6ea24102F49b931DEeeb931f6E32405d',
+    pToken: '0x7825e833d495f3d1c28872415a4aee339d26ac88',
+    redeem: '0x69e42a6b62f40b77975fe38ba9712f75c7c75673',
   },
   [bsc.id]: {
-    oftToken: '0x222',
-    pToken: '0x222',
-    redeem: '0x222',
+    oftToken: '0x193f4A4a6ea24102F49b931DEeeb931f6E32405d',
+    pToken: '0xb6c53431608e626ac81a9776ac3e999c5556717c',
+    redeem: '0x4B67D9321CCD4E28D7Ce9ED8654e6bfFb02Ed1b3',
   },
-  [telosTestnet.id]: {
-    oftToken: '0x9db0209270947a780bfd825f805a5b6a30ef3f42',
-    pToken: '0x8289489afb035c4abd5a01ca9e6272480eeeea55',
-    redeem: '0x53046dba0825e84c0c0eeff90737f8b66441d7ca',
-  },
+  // [telosTestnet.id]: {
+  //   oftToken: '0x9db0209270947a780bfd825f805a5b6a30ef3f42',
+  //   pToken: '0x8289489afb035c4abd5a01ca9e6272480eeeea55',
+  //   redeem: '0x53046dba0825e84c0c0eeff90737f8b66441d7ca',
+  // },
 };
 
 
@@ -42,7 +42,8 @@ export const logoURLforConnector: { [conntector: string]: string } = {
 }
 
 export default createConfig({
-  chains: [mainnet, bsc, telosTestnet],
+  // chains: [mainnet, bsc, telosTestnet],
+  chains: [mainnet, bsc],
   connectors: [
     metaMask(),
     safe(),
@@ -52,6 +53,24 @@ export default createConfig({
   transports: {
     [mainnet.id]: http(),
     [bsc.id]: http(),
-    [telosTestnet.id]: http(),
+    // [telosTestnet.id]: http(),
   },
 })
+
+/*
+pTokens:
+
+Ethereum: 0x7825e833d495f3d1c28872415a4aee339d26ac88
+BSC: 0xb6c53431608e626ac81a9776ac3e999c5556717c
+
+
+OFT:
+
+Ethereum: 0x193f4A4a6ea24102F49b931DEeeb931f6E32405d
+BSC: 0x193f4A4a6ea24102F49b931DEeeb931f6E32405d
+
+Redeem:
+Ethereum: 0x69e42a6b62f40b77975fe38ba9712f75c7c75673
+BSC: 0x4B67D9321CCD4E28D7Ce9ED8654e6bfFb02Ed1b3
+
+ */
